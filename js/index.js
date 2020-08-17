d395c76f1373801d48e60ffdd544e0c1b01a4af6
erikftaylor <30609814+erikftaylor@users.noreply.github.com>
2020-08-16 22:48:47 -0600

    Established root Index.html, CSS and JS folder structure.

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$("button").hover(function(){
+  $(this).css("background-color", "#F2A172");
+  }, function(){
+  $(this).css("background-color", "#044D8C");
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
